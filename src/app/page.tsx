0050da57e7b2ba8a2523146be62defe71cada024
Rigelkukoy <cabayarigel1@gmail.com>
2025-06-10 02:30:28 +0800

    feat: added auto class sorting and changed global font to Poppins

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div className="mx-auto text-center rounded shadow-lg text-white p-2 w-64 bg-green-500"></div>
+  );
 }
